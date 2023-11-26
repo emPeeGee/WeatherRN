f6e778b3b1a156653989df80bb99a0500b30479d
emPeeGee <eempeegee@gmail.com>
2023-11-26 18:36:01 +0200

    feat:  🦧 Add navigation

--- App.tsx
+++ App.tsx
@@ -1,12 +1,7 @@
-import { StatusBar } from 'expo-status-bar';
-import { Text, View } from 'react-native';
-
+import AppNavigation from "./navigation/AppNavigation";
 
 export default function App() {
   return (
-    <View className="flex-1 items-center justify-center">
-      <Text>Open up App.tsx to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <AppNavigation />
   );
 }
---
+++ screens/HomeScreen.tsx
@@ -0,0 +1,12 @@
+import { View, Text, } from 'react-native'
+import React from 'react'
+import { StatusBar } from 'expo-status-bar'
+
+export const HomeScreen = () => {
+  return (
+    <View className='flex-1 relative'>
+    <StatusBar style='light' />
+    {/* <Image source={require} */}
+    </View>
+  )
+}
